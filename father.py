d5e0eb219382ab1bf999e4a989de39d266365aeb
joão <74837451+slocknad@users.noreply.github.com>
2022-01-30 17:26:12 +0000

    Update father.py

--- father.py
+++ father.py
@@ -37,10 +37,17 @@ while True:
         elif " notscramble" in reply_tweet.text:
             notscramble = True
         else:
-            print("No new tweets mentioning father.")
-            sleep(60)
             continue
-    
+
+        if reply_id not in replied_tweets:
+            replied_tweets.append(reply_id)
+            if len(replied_tweets) > 20:
+                replied_tweets.pop(0)
+            open("D:/github/father-klingot/replied_tweets", "r+").truncate(0)
+            open("D:/github/father-klingot/replied_tweets", "r+").write(' '.join([str(tweet) for tweet in replied_tweets]))
+        else:
+            continue
+            
         if reply_tweet._json["in_reply_to_status_id"]:
             tweet = client.get_tweet(id=reply_tweet._json["in_reply_to_status_id"])
             tweet.data.text = re.sub(r"@\w+[ |\n]", "", tweet.data.text)
@@ -53,84 +60,74 @@ while True:
             setattr(tweet.data, 'id', tweet.id)
             setattr(tweet.data, 'text', tweet.text)
 
-        #if not(tweet.data):
-        #    print("No new tweets mentioning father.")
-        #    sleep(60)
-        #    continue
-        if tweet.data.id not in replied_tweets:
-            replied_tweets.append(tweet.data.id)
-            if len(replied_tweets) > 20:
-                replied_tweets.pop(0)
-            open("D:/github/father-klingot/replied_tweets", "r+").truncate(0)
-            open("D:/github/father-klingot/replied_tweets", "r+").write(' '.join([str(tweet) for tweet in replied_tweets]))
-            if scramble:
-                shuffle(languages)
-                scrambled = tweet.data.text
-                for language in languages[:9]:
-                    try:
-                        scrambled = translator.translate(scrambled, dest=language).text
-                    except:
-                        if scrambled != tweet.data.text:
-                            client.create_tweet(text=scrambled[:279])
-                            client.create_tweet(text=scrambled[:279], in_reply_to_tweet_id=reply_id)
-                            print("Replied.")
-                        else:
-                            client.create_tweet(text="there was an error generating my response :(", in_reply_to_tweet_id=reply_id)
-                            print("Translation error.")
-                            break
-                else:
-                    scrambled = translator.translate(scrambled, dest="en").text
-                    scrambled_list = scrambled.split()
-                    reply_tweet = str()
-                    profile_tweet = None
-                    for word in scrambled_list:
-                        reply_tweet += " " + word
-                        if len(reply_tweet) > 280:
-                            reply_tweet = reply_tweet.replace(word, "")
-                            reply_tweet = reply_tweet.replace(".", ". ")
-                            if profile_tweet:
-                                profile_tweet = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=int(profile_tweet.data["id"]))
-                            else:
-                                profile_tweet = client.create_tweet(text=reply_tweet)
-                            reply = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=reply_id)
-                            reply_id = int(reply.data["id"])
-                            reply_tweet = word
-                            break
-                        else:
-                            client.create_tweet(text=scrambled)
-                            client.create_tweet(text=scrambled, in_reply_to_tweet_id=reply_id)
-                    print("Replied.")
-    
-            elif notscramble:
-                notscrambled = requests.post(
-                "https://api.deepai.org/api/text-generator",
-                data={
-                    'text': tweet.data.text,
-                },
-                headers={'api-key': 'ebb6d07f-9985-460f-95f7-96d836b8a7c5'}
-                )
+        if scramble:
+            shuffle(languages)
+            scrambled = tweet.data.text
+            for language in languages[:9]:
                 try:
-                    notscrambled = notscrambled.json()['output']
+                    scrambled = translator.translate(scrambled, dest=language).text
                 except:
-                        print(notscrambled.json())
+                    if scrambled != tweet.data.text:
+                        client.create_tweet(text=scrambled[:279])
+                        client.create_tweet(text=scrambled[:279], in_reply_to_tweet_id=reply_id)
+                        print("Replied.")
+                    else:
+                        client.create_tweet(text="there was an error generating my response :(", in_reply_to_tweet_id=reply_id)
+                        print("Translation error.")
+                        break
+            else:
+                scrambled = translator.translate(scrambled, dest="en").text
+                scrambled_list = scrambled.split()
+                reply_tweet = str()
+                profile_tweet = None
+                for word in scrambled_list:
+                    reply_tweet += " " + word
+                    if len(reply_tweet) > 280:
+                        reply_tweet = reply_tweet.replace(word, "")
+                        reply_tweet = reply_tweet.replace(".", ". ")
+                        if profile_tweet:
+                            profile_tweet = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=int(profile_tweet.data["id"]))
+                        else:
+                            profile_tweet = client.create_tweet(text=reply_tweet)
+                        reply = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=reply_id)
+                        reply_id = int(reply.data["id"])
+                        reply_tweet = word
+                        break
                 else:
-                    notscrambled = notscrambled.replace(tweet.data.text,"")
-                    notscrambled_list = notscrambled.split()
-                    reply_tweet = str()
-                    profile_tweet = None
-                    for word in notscrambled_list:
-                        reply_tweet += " " + word
-                        if len(reply_tweet) > 280:
-                            reply_tweet = reply_tweet.replace(word, "")
-                            reply_tweet = reply_tweet.replace(".", ". ")
-                            if profile_tweet:
-                                profile_tweet = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=int(profile_tweet.data["id"]))
-                            else:
-                                profile_tweet = client.create_tweet(text=reply_tweet)
-                            reply = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=reply_id)
-                            reply_id = int(reply.data["id"])
-                            reply_tweet = word
-                    print("Replied.")
-        else:
-            print("No new tweets mentioning father.")
-        sleep(60)
+                    client.create_tweet(text=scrambled)
+                    client.create_tweet(text=scrambled, in_reply_to_tweet_id=reply_id)
+                print("Replied:", scrambled)
+
+        elif notscramble:
+            notscrambled = requests.post(
+            "https://api.deepai.org/api/text-generator",
+            data={
+                'text': tweet.data.text,
+            },
+            headers={'api-key': 'ebb6d07f-9985-460f-95f7-96d836b8a7c5'}
+            )
+            try:
+                notscrambled = notscrambled.json()['output']
+            except:
+                    print(notscrambled.json())
+            else:
+                notscrambled = notscrambled.replace(tweet.data.text,"")
+                notscrambled_list = notscrambled.split()
+                reply_tweet = str()
+                profile_tweet = None
+                for word in notscrambled_list:
+                    reply_tweet += " " + word
+                    if len(reply_tweet) > 280:
+                        reply_tweet = reply_tweet.replace(word, "")
+                        reply_tweet = reply_tweet.replace(".", ". ")
+                        if profile_tweet:
+                            profile_tweet = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=int(profile_tweet.data["id"]))
+                        else:
+                            profile_tweet = client.create_tweet(text=reply_tweet)
+                        reply = client.create_tweet(text=reply_tweet, in_reply_to_tweet_id=reply_id)
+                        reply_id = int(reply.data["id"])
+                        reply_tweet = word
+                print("Replied:", notscrambled)
+
+    print("No new tweets mentioning father.")
+    sleep(60)
